9124c60dd32c22f6b08bfe26b8bb400d3bbd2909
Charlie Thomson <charliecthomson@gmail.com>
2020-12-09 07:45:15 -0500

    fmt, add .gitignore + .rustfmt.toml

--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,5 @@
+use log::{debug, LevelFilter};
 use std::path::*;
-use log::{
-    LevelFilter,
-    debug};
-
 
 pub fn errlog<P: AsRef<Path>>(config_dir: P, message: String) {
     let error_file = "log.log";
@@ -10,7 +7,14 @@ pub fn errlog<P: AsRef<Path>>(config_dir: P, message: String) {
     let dir = error_dir.to_str().unwrap();
 
     match simple_logging::log_to_file(dir, LevelFilter::Debug) {
-        Err(e) => throw_error(format!("ERROR!\nNo permission to write to \"{}\" directory with error {}!", config_dir.as_ref().to_str().unwrap(), e).as_str()),
+        Err(e) => throw_error(
+            format!(
+                "ERROR!\nNo permission to write to \"{}\" directory with error {}!",
+                config_dir.as_ref().to_str().unwrap(),
+                e
+            )
+            .as_str(),
+        ),
         _ => (),
     };
     debug!("{}", message);
@@ -18,7 +22,7 @@ pub fn errlog<P: AsRef<Path>>(config_dir: P, message: String) {
     return;
 }
 
-pub fn throw_error(message : &str){
+pub fn throw_error(message: &str) {
     print!("{}", message);
     std::process::exit(1);
 }
