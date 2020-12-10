52d28cc9fa4801cc782e9470de1f5b3c9bfd03eb
Samuel Rembisz <sjrembisz07@gmail.com>
2020-12-09 23:08:11 -0500

    Started work on term and collector

--- src/event.rs
+++ src/event.rs
@@ -1,4 +1,19 @@
+use std::time::{Duration, SystemTime};
+
 pub enum Event {
     Flag(bool),
     Wait,
+}
+impl Event {
+    pub fn wait(&mut self, time : f64) {
+        let now = SystemTime::now();
+        while now.elapsed().unwrap() < Duration::from_secs_f64(time) {}
+    }
+
+    pub fn is_set(self) -> bool {
+        match self {
+            Event::Flag(true) => return true,
+            _ => return false,
+        };
+    }
 }
