c7c38cafcbda0ee217b3a51a5ccb7d87df107db2
Charlie Thomson <charliecthomson@gmail.com>
2020-12-14 13:29:58 -0500

    Add symbol, some other things :)

--- from_map/src/lib.rs
+++ from_map/src/lib.rs
@@ -5,4 +5,10 @@ pub trait FromMap {
     fn from_map(map: HashMap<String, Self::Value>) -> Self;
 }
 
+pub trait FromMapDefault: Default {
+    type Value: Default + Copy + From<String>;
+    fn from_map_default(map: HashMap<String, Self::Value>) -> Self;
+    fn default_map() -> HashMap<String, Self::Value>;
+}
+
 pub use from_map_derive::*;
--- from_map_derive/src/lib.rs
+++ from_map_derive/src/lib.rs
@@ -22,6 +22,24 @@ fn get_items(item: &syn::DeriveInput) -> Vec<proc_macro2::Ident> {
     items
 }
 
+fn get_defaults(item: &syn::DeriveInput) -> Vec<(proc_macro2::Ident, Option<String>)> {
+    match item.data.clone() {
+        syn::Data::Struct(syn::DataStruct {
+            fields: syn::Fields::Named(syn::FieldsNamed { named, .. }),
+            ..
+        }) => named
+            .into_iter()
+            .map(|field| {
+                (
+                    field.ident.unwrap(),
+                    field.attrs.get(0).map(attr_str).flatten(),
+                )
+            })
+            .collect(),
+        _ => unreachable!(),
+    }
+}
+
 fn attr_ident(attr: &syn::Attribute) -> Option<&syn::Ident> {
     let syn::Path { segments, .. } = &attr.path;
     if let Some(syn::PathSegment { ident, .. }) = segments.iter().next() {
@@ -29,6 +47,15 @@ fn attr_ident(attr: &syn::Attribute) -> Option<&syn::Ident> {
     } else {
         None
     }
+}
+fn attr_str(attr: &syn::Attribute) -> Option<String> {
+    format!(
+        "{}",
+        syn::parse::<TokenStream2>(TokenStream::from(attr.tokens.clone())).unwrap()
+    )
+    .split('"')
+    .nth(1)
+    .map(<_ as ToString>::to_string)
 }
 fn attr_lit(attr: &syn::Attribute) -> Option<proc_macro2::Literal> {
     match attr.tokens.clone().into_iter().nth(1) {
@@ -53,7 +80,7 @@ fn get_value_ty(item: &syn::DeriveInput) -> Option<syn::Ident> {
 }
 
 #[proc_macro_derive(FromMap, attributes(value_type))]
-pub fn derive_into_query(input: TokenStream) -> TokenStream {
+pub fn derive_from_map(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let items = get_items(&input);
     let value_type = get_value_ty(&input);
@@ -79,6 +106,69 @@ pub fn derive_into_query(input: TokenStream) -> TokenStream {
                 Self {
                     #body
                 }
+            }
+        }
+    };
+    gen.into()
+}
+
+#[proc_macro_derive(FromMapDefault, attributes(value_type, default))]
+pub fn derive_from_map_default(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let items = get_items(&input);
+    let value_type = get_value_ty(&input);
+    let struct_ident = &input.ident;
+    let defaults = get_defaults(&input);
+
+    let default_map = defaults
+        .iter()
+        .fold(TokenStream2::new(), |mut acc, (ident, default)| {
+            let default = default.clone().unwrap_or_default();
+            acc.extend(
+                quote! {
+                    (stringify!(#ident), #default),
+                }
+                .into_iter(),
+            );
+            acc
+        });
+    let body = items
+        .iter()
+        .map(|ident| {
+            quote! {
+                #ident: map.get(stringify!(#ident).into()).cloned().unwrap_or(default_map.get(stringify!(#ident).into()).cloned().unwrap()),
+            }
+        })
+        .fold(TokenStream2::new(), |mut acc, cur| {
+            acc.extend(cur.into_iter());
+            acc
+        });
+
+    let gen = quote! {
+        impl ::from_map::FromMapDefault for #struct_ident {
+            type Value = #value_type;
+
+            fn from_map_default(map: HashMap<String, Self::Value>) -> Self {
+                let default_map = Self::default_map();
+                Self {
+                    #body
+                }
+            }
+            fn default_map() -> HashMap<String, <Self as FromMapDefault>::Value> {
+                let map = vec![
+                    #default_map
+                ];
+
+                map
+                    .iter()
+                    .map(|(field, colstr)| (field.to_string(), Color::new(colstr).unwrap()))
+                    .collect()
+
+            }
+        }
+        impl Default for #struct_ident {
+            fn default() -> Self {
+                Self::from_map_default(HashMap::new())
             }
         }
     };
