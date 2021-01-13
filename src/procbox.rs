09f3de77c383bf1db9a0123387ee8b15a8fa5d3b
Samuel Rembisz <sjrembisz07@gmail.com>
2021-01-13 13:23:23 -0500

    Finished ProcBox

--- src/procbox.rs
+++ src/procbox.rs
@@ -9,10 +9,12 @@ use {
         fx,
         graph::{Graph, Graphs},
         key::Key,
+        menu::Menu,
         mv,
         proccollector::{
             ProcCollector,
             ProcCollectorDetails,
+            ProcessInfo,
         },
         symbol,
         SYSTEM,
@@ -20,7 +22,10 @@ use {
         theme::{Color, Theme},
     },
     inflector::Inflector,
-    psutil::process::Status,
+    psutil::process::{
+        Status,
+        MemoryInfo,
+    },
     std::{
         collections::HashMap,
         iter::Enumerate,
@@ -48,7 +53,7 @@ pub struct ProcBox {
     detailed_width: u32,
     detailed_height: u32,
     buffer: String,
-    pid_counter: HashMap<i32, i32>,
+    pid_counter: HashMap<u32, u32>,
     redraw : bool,
 }
 impl ProcBox {
@@ -74,7 +79,7 @@ impl ProcBox {
             detailed_width: 0,
             detailed_height: 8,
             buffer: "proc".to_owned(),
-            pid_counter: HashMap::<i32, i32>::new(),
+            pid_counter: HashMap::<u32, u32>::new(),
             redraw : true,
         };
         procbox.parent.x = 1;
@@ -263,6 +268,7 @@ impl ProcBox {
         term: &mut Term,
         draw : &mut Draw,
         proc : &mut ProcCollector,
+        menu : &mut Menu,
     ) {
         if self.parent.stat_mode {
             return;
@@ -285,7 +291,7 @@ impl ProcBox {
         let mut h: u32 = self.current_h - 2;
         let mut prog_len: usize = 0;
         let mut arg_len: usize = 0;
-        let mut val: i32 = 0;
+        let mut val: u32 = 0;
         let mut c_color: String = String::default();
         let mut m_color: String = String::default();
         let mut t_color: String = String::default();
@@ -1426,8 +1432,386 @@ impl ProcBox {
                 is_selected = false;
             }
 
-            indent = items.get
+            let indent = match items.get("indent".to_owned()).unwrap_or(ProcessInfo::None) {
+                ProcessInfo::String(s) => s.clone(),
+                ProcessInfo::None => String::default(),
+                _ => {
+                    errlog("Malformed type in items['indent']".to_owned());
+                    String::default()
+                }
+            };
+
+            let name = match items.get("name".to_owned()).unwrap_or(ProcessInfo::None) {
+                ProcessInfo::String(s) => s.clone(),
+                ProcessInfo::None => String::default(),
+                _ => {
+                    errlog("Malformed type in items['name']".to_owned());
+                    String::default()
+                }
+            };
+
+            let cmd = match items.get("cmd".to_owned()).unwrap_or(ProcessInfo::None) {
+                ProcessInfo::String(s) => s.clone(),
+                ProcessInfo::Non => String::default(),
+                _ => {
+                    errlog("Malformed type in items['cmd']".to_owned());
+                    String::default()
+                }
+            };
+
+            let threads : u64 = match items.get("threads".to_owned()).unwrap_or(ProcessInfo::None) {
+                ProcessInfo::U64(u) => u.clone(),
+                ProcessInfo::None => 0,
+                _ => {
+                    errlog("Malformed type in items['threads']".to_owned());
+                    0
+                }
+            };
+
+            let username : String = match items.get("username".to_owned()).unwrap_or(ProcessInfo::None) {
+                ProcessInfo::String(s) => s.clone(),
+                ProcessInfo::None => String::default(),
+                _ => {
+                    errlog("Malformed type in items['username']".to_owned());
+                    String::default()
+                }
+            };
+
+            let mem : MemoryInfo = match items.get("mem".to_owned()).unwrap_or(ProcessInfo::None) {
+                ProcessInfo::MemoryInfo(m) => m.clone(),
+                ProcessInfo::None => MemoryInfo {
+                    rss : 0,
+                    vms : 0,
+                },
+                _ => {
+                    errlog("Malformed type in items['mem']".to_owned());
+                    MemoryInfo {
+                        rss : 0,
+                        vms : 0,
+                    }
+                }
+            };
+
+            let mem_b : Bytes = match items.get("mem_b".to_owned()).unwrap_or(ProcessInfo::None) {
+                ProcessInfo::U64(u) => u.clone(),
+                ProcessInfo::None => 0,
+                _ => {
+                    errlog("Malformed type in items['mem_b']".to_owned());
+                    0
+                }
+            };
+
+            let cpu : f32 = match items.get("cpu".to_owned()).unwrap_or(ProcessInfo::None) {
+                ProcessInfo::F32(f) => f.clone(),
+                ProcessInfo::None => 0.0,
+                _ => {
+                    errlog("Malformed type in items['cpu']".to_owned());
+                    0.0
+                }
+            };
+
+            if CONFIG.proc_tree {
+                arg_len = 0;
+                let size_set = format!("{}{}", indent, pid).len();
+                offset = size_set as u32;
+                tree_len = size_set;
+
+                indent = format!("{:.width$}", indent, width = tree_len - pid.to_string().len());
+                if offset - name.len() as u32 > 12 {
+                    let cmd_splitter = cmd.split(" ")[0].split("/");
+                    cmd = cmd_splitter[cmd.len() - 2];
+                    if !cmd.starts_with(name) {
+                        offset = name.len() as u32;
+                        arg_len = (tree_len - format!("{}{} {} ", indent, pid, name).len() as u32 + 2) as usize;
+                        cmd = format!("({})", cmd[..arg_len - 4]);
+                    }
+                }
+            } else {
+                offset = prog_len as u32 - 1;
+            }
+            if cpu > 1.0 || graphs.pid_cpu.contains_key(pid) {
+                if !graph.pid_cpu.contains_key(pid) {
+                    graphs.pid_cpu.insert(pid,Graph::new(5, 1, None, vec![0], term, false, 0, 0, None));
+                    self.pid_counter.insert(pid, 0);
+                } else if cpu < 1.0 {
+                    self.pid_counter.get_mut(pid).unwrap() += 1;
+                    if self.pid_counter[pid] > 10 {
+                        self.pid_counter.remove(pid);
+                        graphs.pid_cpu.remove(pid);
+                    }
+                } else {
+                    self.pid_counter.insert(pid, 0);
+                }
+            }
+
+            end = if CONFIG.proc_colors {
+                format!("{}{}", THEME.colors.main_fg, fx::ub)
+            } else {
+                fx::ub.to_owned()
+            };
+            if self.selected > cy {
+                calc = self.selected as u32 - cy;
+            } else if 0 < self.selected && self.selected <= cy {
+                calc = cy - self.selected as u32;
+            } else {
+                calc = cy;
+            }
+            if CONFIG.proc_colors && !is_selected {
+                vals = Vec::<String>::new();
+                for v in vec![cpu as u32, mem as u32, (threads / 3) as u32] {
+                    if CONFIG.proc_gradient {
+                        val = (if v <= 100 {
+                            v
+                        } else {
+                            100
+                        } + 100) - calc * 100 / self.select_max as u32;
+                        vals.push(
+                            THEME.gradient[
+                                if v < 100 {
+                                    "proc_color".to_owned()
+                                } else {
+                                    "process".to_owned()
+                                }
+                            ][
+                                if val < 100 {
+                                    val
+                                } else {
+                                    val - 100
+                                }
+                            ]
+                            .clone()
+                        );
+                    } else {
+                        vals.push(
+                            THEME.gradient["process".to_owned()][
+                                if v <= 100 {
+                                    v
+                                } else {
+                                    100
+                                }
+                            ]
+                        );
+                    }
+                }
+                c_color = vals;
+                m_color = vals;
+                t_color = vals;
+            } else {
+                c_color = fx::b.to_owned();
+                m_color = fx::b.to_owned();
+                t_color = fx::b.to_owned();
+            }
+            if CONFIG.proc_gradient && !is_selected {
+                g_color = THEME.gradient["proc".to_owned()][calc as usize * 100 / self.select_max].clone();
+            }
+            if is_selected {
+                c_color = String::default();
+                m_color = String::default();
+                t_color = String::default();
+                g_color = String::default();
+                end = String::default();
+                out.push_str(format!("{}{}{}", THEME.colors.selected_bg, THEME.colors.selected_fg, fx::b).as_str());
+            }
+
+            // * Creates one line for a process with all gathered information
+            out.push_str(format!("{}{}{}{:>width$} {}{:<offset1$.offset2$} {}{}{}{}{}{}{}",
+                    mv::to(y + cy, x),
+                    g_color,
+                    indent,
+                    pid,
+                    c_color,
+                    name,
+                    end,
+                    if arg_len {
+                        format!("{}{:<arg_len1$.arg_len2$}", g_color, cmd, arg_len1 = arg_len, arg_len2 = arg_len - 1)
+                    } else {
+                        String::default()
+                    },
+                    if tr_show {
+                        t_color + if threads < 1000 {
+                            format!("{:>4}", threads).as_str()
+                        } else {
+                            "999> "
+                        } + end.as_str()
+                    } else {
+                        String::default()
+                    },
+                    if usr_show {
+                        g_color + if username.len() < 10 {
+                            format!("{:<9.9}", username)
+                        } else {
+                            format!("{:<8}", username[..8])
+                        }.as_str()
+                    } else {
+                        String::default()
+                    },
+                    m_color + (
+                        if !CONFIG.proc_mem_bytes {
+                            if mem < 100 {
+                                format!("{:>4.1f}", mem)
+                            } else {
+                                format!("{4.0f}", mem)
+                            }
+                        } else {
+                            format!("{:>4.4}", floating_humanizer(mem_b, true, false, 0, false))
+                        }
+                    ).as_str() + end.as_str(),
+                    format!(" {}{}{}{}{}", THEME.colors.inactive_fg, ".".repeat(5), THEME.colors.main_fg, g_color, c_color) + if cpu < 100 {
+                        format!(" {:>4.1f} ", cpu)
+                    } else {
+                        format!("{:>5.0f} ", cpu)
+                    }.as_str() + end.as_str(),
+                    if proc.num_procs > self.select_max as u32 {
+                        " "
+                    } else {
+                        ""
+                    },
+                    width = if CONFIG.proc_tree {
+                        1
+                    } else {
+                        7
+                    },
+                    offset1 = offset,
+                    offset2 = offset,
+                )
+                .as_str()
+            );
+
+            // * Draw small cpu graph for process if cpu usage was above 1% in the last 10 updates
+            if graphs.pid_cpu.contains_key(&pid) {
+                out.push_str(format!("{}{}{}{}",
+                        mv::to(y + cy, x + w - if proc.num_procs > self.select_max as u32 {
+                            12
+                        } else {
+                            11
+                        }),
+                        if CONFIG.proc_colors {
+                            c_color
+                        } else {
+                            THEME.colors.proc_misc
+                        },
+                        graphs.pid_cpu[pid].call(if self.moved {
+                            None
+                        } else {
+                            Some(cpu.round() as i32)
+                        }, term),
+                        THEME.colors.main_fg,
+                    )
+                    .as_str()
+                );
+            }
+
+            if is_selected {
+                out.push_str(format!("{}{}{}{}{}",
+                        fx::ub,
+                        term.fg,
+                        term.bg,
+                        mv::to(y + cy, x + w - 1),
+                        if proc.num_procs > self.select_max {
+                            " "
+                        } else {
+                            ""
+                        },
+                    )
+                    .as_str()
+                );
+            }
+
+            cy += 1;
+            if cy == h {
+                break;
+            }
         }
-        
+        if cy < h {
+            for i in 0..h-cy {
+                out.push_str(format!("{}{}", mv::to(y + cy + i, x), " ".repeat(w)).as_str())
+            }
+        }
+
+        // * Draw scrollbar if needed
+        if proc.num_procs > self.select_max as u32 {
+            if self.resized {
+                match key.mouse.get_mut("mouse_scroll_up".to_owned()) {
+                    Some(v) => {
+                        let mut top = Vec::<Vec<i32>>::new();
+                        for i in 0..3 {
+                            let mut adder = Vec::<i32>::new();
+                            adder.push((x + w - 2 + i) as i32);
+                            adder.push(y as i32);
+                            top.push(adder);
+                        }
+                        v = top.clone();
+                    },
+                    None => {
+                        errlog("key.mouse does not have 'mouse_scroll_up'!");
+                        ()
+                    }
+                };
+                match key.mouse.get_mut("mouse_scroll_down".to_owned()) {
+                    Some(v) => {
+                        let mut top = Vec::<Vec<i32>>::new();
+                        for i in 0..3 {
+                            let mut adder = Vec::<i32>::new();
+                            adder.push((x + w - 2 + i) as i32);
+                            adder.push((y + h - 1) as i32);
+                            top.push(adder);
+                        }
+                        v = top.clone();
+                    },
+                    None => {
+                        errlog("key.mouse does not have 'mouse_scroll_down'!");
+                        ()
+                    }
+                };
+            }
+            scroll_pos = (self.start * (self.select_max as i32 - 2) / (proc.num_procs - (self.select_max as u32 - 2)) as i32).try_into::<u32>().unwrap_or(0);
+            if scroll_pos > h - 3 || self.start >= (proc.num_procs - self.select_max as u32) as i32 {
+                scroll_pos = h - 3;
+            }
+            out.push_str(format!("{}{}{}↑{}↓{}{}█"
+                    mv::to(y, x + w - 1),
+                    fx::b,
+                    THEME.colors.main_fg,
+                    mv::to(y + h - 1, x + w - 1),
+                    fx::ub,
+                    mv::to(y + 1 + scroll_pos, x + w - 1),
+                )
+                .as_str()
+            );
+        } else if key.mouse.contains_key(&"scroll_up".to_owned()) {
+            key.mouse.remove(&"scroll_up".to_owned());
+            key.mouse.remove(&"scroll_down".to_owned());
+        }
+
+        // * Draw current selection and number of processes
+        out.push_str(format!("{}{}{}{}{}{}{}{}",
+                mv::to(y + h, x + w - 3 - loc_string.len() as u32),
+                THEME.colors.proc_box,
+                symbol::title_left,
+                THEME.colors.title,
+                fx::b,
+                loc_string,
+                fx::ub,
+                THEME.colors.proc_box.call(symbol::title_right.to_owned(), term),
+            )
+            .as_str()
+        );
+
+        // * Clean up dead processes graphs and counters
+        self.count += 1;
+        if self.count == 100 {
+            self.count = 0;
+            for (pid, _) in self.pid_counter {
+                if !psutil::process::exists(pid) {
+                    self.pid_counter.remove(&p);
+                    graphs.pid_cpu.remove(&p);
+                }
+            }
+        }
+
+        draw.buffer(self.buffer, vec![format!("{}{}{}", out_misc, out, term.fg)], false, false, 100, menu.active, false, false, key);
+        self.redraw = false;
+        self.parent.resized = false;
+        self.moved = false;
     }
 }
